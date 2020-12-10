7cea3137d17093e049935d9891c3fb6003e68c0b
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-12-10 07:45:25 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var dog, happyDog, database, foodS, foodStock, dogImage, happyDogImage;
+
+function preload(){
+  dogImage = loadImage("images/dogImg.png");
+  happyDogImage = loadImage("images/dogImg1.png");
+}
+
+function setup() {
+  createCanvas(500, 500);
+  dog = createSprite(200, 200, 20, 20);
+  dog.addImage(dogImage);
+  happyDog = createSprite(300, 300, 20, 20);
+  happyDog.addImage(happyDogImage);
+  database = firebase.database();
+  foodStock = database.ref("Food");
+  foodStock.on("value", readStock);
+  
+}
+
+
+function draw() {  
+  background(46,139,87);
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(happyDog);
+  }
+  drawSprites();
+  fill("#ff177c");
+  stroke("#50d4e6");
+  textSize(15);
+  text("Food stock:" +foodStock, 250, 400);
+}
+
+function readStock(data){
+  foodS = data.val();
+}
+
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  } else{
+    x = x-1
+  }
+  databaase.ref("/").update({
+    Food:x
+  })
+}
+
+
+
