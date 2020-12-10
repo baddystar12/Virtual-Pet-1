246696d68aec285c5ef329a0568c979fc0a0fc36
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-12-10 09:14:59 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,8 +9,10 @@ function setup() {
   createCanvas(500, 500);
   dog = createSprite(200, 200, 20, 20);
   dog.addImage(dogImage);
+  dog.scale = 0.1;
   happyDog = createSprite(300, 300, 20, 20);
   happyDog.addImage(happyDogImage);
+  happyDog.scale = 0.1;
   database = firebase.database();
   foodStock = database.ref("Food");
   foodStock.on("value", readStock);
@@ -28,7 +30,7 @@ function draw() {
   fill("#ff177c");
   stroke("#50d4e6");
   textSize(15);
-  text("Food stock:" +foodStock, 250, 400);
+  text("Food stock:" +foodStock, 20, 400);
 }
 
 function readStock(data){
